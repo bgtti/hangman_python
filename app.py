6284503a77a1be363313b51e4aabd71c34bfa62c
bgtti <bru.mgtti@gmail.com>
2023-01-09 15:12:37 +0100

    final

--- app.py
+++ app.py
@@ -1,16 +1,63 @@
-# Hangman
+# Hangman game
 import random
 
 words = ["elephant", "yellow", "Zimbabwe", "mountain", "circus"]
-word_chosen = ""
+word_chosen = []
+word_printed = []
 num_letters = 0
 wrong_guesses = []
-right_guesses = []
+
+
+def the_game_loop():
+    global word_chosen
+    word_chosen = []
+    global word_printed
+    word_printed = []
+    global wrong_guesses
+    wrong_guesses = []
+    game_initiation()
+
+
+def replay():
+    print("Would you like to play again?")
+    print("Type 'replay' to play again or 'exit' to stop.")
+    replay_answer = input()
+    if replay_answer == "replay":
+        print("***New game started***\n")
+        the_game_loop()
+    else:
+        print("***Thank you for playing, goodbye!***\n")
+
+
+def check_game():
+    if len(wrong_guesses) == 6:
+        print("***** Game over: you lose! *****\n")
+        replay()
+    elif word_chosen == word_printed:
+        print("***** Game over: you win! *****\n")
+        replay()
+    else:
+        game()
+
+
+def game():
+    print("Guess a letter: ")
+    guess = input()
+    if guess in word_chosen or guess.lower() in word_chosen or guess.upper() in word_chosen:
+        for i in range(num_letters):
+            if guess.lower() == word_chosen[i].lower():
+                global word_printed
+                word_printed[i] = word_chosen[i]
+    else:
+        wrong_guesses.append(guess.lower())
+    drawing()
 
 
 def drawing():
+    print("===========================")
     num_wrong_guesses = len(wrong_guesses)
-    print("_ " * num_letters + "\n")
+    print(" ".join(word_printed))
+    print()
     if num_wrong_guesses > 0:
         print("Your wrong guesses: " + ', '.join(wrong_guesses))
     print(" +---+")
@@ -21,8 +68,8 @@ def drawing():
             print("     |")
             print("     |")
         elif num_wrong_guesses == 2:
-            print(" |    |")
-            print("      |")
+            print(" |   |")
+            print("     |")
         elif num_wrong_guesses == 3:
             print("/|   |")
             print("     |")
@@ -43,19 +90,19 @@ def drawing():
 
     print("     |")
     print()
-
-
-def user_guess():
-    guess = input()
+    check_game()
 
 
 def game_initiation():
-    word_chosen = random.choice(words)
-    num_letters = len(word_chosen)
-    # print(word_chosen)
-    print("Welcome to Hangman.\n Choose a letter to start.")
-    print("_ " * len(word_chosen))
-    guess = input()
+    global word_chosen
+    word_chosen = list(random.choice(words))
+    global num_letters
+    num_letters = int(len(word_chosen))
+    for i in range(num_letters):
+        word_printed.append("_")
+    print("===========================")
+    print("\n*  Welcome to Hangman.  *\nGuess a letter to start.\n")
+    drawing()
 
 
-game_initiation()
+the_game_loop()
